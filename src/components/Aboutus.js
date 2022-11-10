b1fa294d34753f57c72f07164e5ade1199c30989
Saket <sakettiwari076@gmail.com>
2022-11-10 04:47:10 -0800

    updates

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -1,38 +1,34 @@
 import React from 'react'
 
-const Try = () => {
+const Aboutus = () => {
   return (
     <div>
-        <div className='fonthead mainfont lg:mx-20 sm:mx-10 '>
-            <div className='text-black     fonthead'>
-                <p className='text-center text-5xl textshadow   font-bold letterspace    text-black  transition duration-1000 ease-in-out
-                       '> <span className='border-2   textshadow'>About us</span> </p>
-            </div>
-<div className=' grid text-black lg:grid-cols-2'>
-    <div className=' px-6  h-screen'>
-        <p className=' letterspace font-bold py-32 lg:text-2xl text-lg leading-10   '> <span className ="red">Anonymous coders KIET</span>  is a student body of KIET , formed in 2022 with the aim to remove the differences of </p>
-<button className="bg-red font-bold  px-7 py-3 shadow-lg text-black bg-violet-400  rounded-lg shadow-violet-500/50 ...">Subscribe</button>
+        <div className='grid pt-10 mainfont lg:grid-cols-2 mx-10'>
+    <div className='justify-center pt-8 lg:pl-32'>
+        <h1 className='text-2xl font-semibold'>About us</h1>
+        <p className='text-gray-900 pt-5 font-normal'>      We are a group of excited people with a clear sense of purpose. We believe in continuous learning and development. We are adaptive to upcoming challenges in technology and management. We learn, code, preach, and grow together. Here at DSC KIET, we are not just a team, we are a growing family of developers.
+</p>
+<p className=''>Our members active on </p>
+<div className='flex gap-4 pt-4'>
+<img src='github.webp' className='h-10'/>
+<img src='gfg.webp' className='h-10'/>
+<img src='codeshef.webp' className='h-10'/>
+<img src='codeforces.webp' className='h-10'/>
 
+    <img src='leetcode.webp' className='h-10'/>
+</div>
     </div>
-    <div className=' py-9 grid h-screen  sm:space-y-14   '>
-{/* <div className='text-6xl   text-white lg:pt-12    text-center'>898</div>
-<div className='text-6xl   text-white   text-center'>898</div>
-<div className='text-6xl text-white  text-center'>898</div>
-<div className='text-6xl   text-white  text-center'>898</div> */}
-<img src='realabout.gif' className='mt-20'/>
-
-
-
-
+    <div className=''>
+      <img src='realabout.gif' className='imageteam'></img>
     </div>
+
+
 
 
 </div>
-</div>
-        
+
     </div>
-    
   )
 }
 
-export default Try
+export default Aboutus
--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import Team from './components/Team';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Homepage/> */}
-    {/* <Aboutus/> */}
+    <Homepage/>
+    <Aboutus/>
     <Team/>
     {/* <Contactus/> */}
-    {/* <Footer/> */}
+    <Footer/>
     {/* <App /> */}
   </React.StrictMode>
 );
