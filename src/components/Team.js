9800b8b42d864eb9376b60bfdbe3959284f64460
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-11-10 22:07:37 +0530

    name updated

--- src/components/Team.js
+++ src/components/Team.js
@@ -39,7 +39,7 @@ const Team = () => {
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84"/>
           <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Vaibhav Bisht</h2>
+            <h2 className="text-gray-900 title-font font-medium">Aryan Tiwari</h2>
             <p className="text-gray-500">Soon</p>
           </div>
         </div>
@@ -48,7 +48,7 @@ const Team = () => {
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88"/>
           <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Aryan Tiwari</h2>
+            <h2 className="text-gray-900 title-font font-medium">Vaibhav Bisht</h2>
             <p className="text-gray-500">Soon</p>
           </div>
         </div>
@@ -75,7 +75,7 @@ const Team = () => {
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98"/>
           <div className="flex-grow">
-            <h2 className=" text-gray-900 title-font font-medium">Om Daryani</h2>
+            <h2 className=" text-gray-900 title-font font-medium">Prabhat Sahrawat</h2>
             <p className="text-gray-500">Soon</p>
           </div>
         </div>
@@ -111,7 +111,7 @@ const Team = () => {
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98"/>
           <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Prabhat Sahrawat </h2>
+            <h2 className="text-gray-900 title-font font-medium"> Om Daryani</h2>
             <p className="text-gray-500">Soon</p>
           </div>
         </div>
