1ae937fbf82b9c50e94ec795d192fe946da5147a
Saket <sakettiwari076@gmail.com>
2022-11-11 13:06:51 -0800

    add

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -8,16 +8,16 @@ const Aboutus = () => {
         <div className='grid pt-10 mainfont lg:grid-cols-2 mx-10'>
     <div className='justify-center pt-8 lg:pl-32'>
         <h1 className='text-2xl font-semibold'>About us</h1>
-        <p className='text-gray-900 pt-5 font-normal'>      We are a group of excited and enthusiastic people who are intrested in technology.We are here to remove the bounderies between CP , DSA , Developmentby making an all around growth in an individual.Moreover we are like a family which supports every individual as per the requirements.This is how we grow together.
+        <p className='text-gray-900 pt-5 font-normal'>      We are a group of excited and enthusiastic people who are intrested in technology.We are here to remove the bounderies between CP , DSA , Development by making an all around growth in an individual.Moreover we are like a family which supports every individual as per the requirements.This is how we grow together.
 </p>
 <p className=''>Our members active on </p>
 <div className='flex gap-4 pt-4'>
-<img src='github.webp' className='h-10'/>
-<img src='gfg.webp' className='h-10'/>
-<img src='codeshef.webp' className='h-10'/>
-<img src='codeforces.webp' className='h-10'/>
+<img src='github.webp' className='h-10 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...'/>
+<img src='gfg.webp' className='h-10 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...'/>
+<img src='codeshef.webp' className='h-10 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...'/>
+<img src='codeforces.webp' className='h-10 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...'/>
 
-    <img src='leetcode.webp' className='h-10'/>
+    <img src='leetcode.webp' className='h-10 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...'/>
 </div>
     </div>
     <div className=''>
--- src/components/Team.js
+++ src/components/Team.js
@@ -119,25 +119,6 @@ const Team = () => {
           </div>
         </div>
       </div>
-      {/* <div className="p-2 lg:w-1/3 md:w-1/2 w-full transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98"/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium"> Om Daryani</h2>
-            <p className="text-gray-500">Soon</p><i class="fa-brands fa-linkedin fa-2x tele"></i>
-            <i class="fa-brands fa-instagram fa-2x insta"></i>
-          </div>
-        </div>
-      </div> */}
-            {/* <div className="p-2 lg:w-1/3 md:w-1/2 w-full transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...">
-        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={gaurav}/>
-          <div className="flex-grow">
-            <h2 className="text-gray-900 title-font font-medium">Gaurav Payal</h2>
-            <p className="text-gray-500">Soon</p><i class="fa-brands fa-linkedin fa-2x tele"></i><i class="fa-brands fa-instagram fa-2x insta"></i>
-          </div>
-        </div>
-      </div> */}
       <div className="p-2 lg:w-1/3 md:w-1/2 w-full transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110   duration-300 ...">
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={ria}/>
