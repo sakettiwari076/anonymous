921b1bd53b21b7bcbaeb540082e930709207847c
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-11-15 00:51:06 +0530

    roadmap updated

--- src/components/Roadmap/Roadmaps.js
+++ src/components/Roadmap/Roadmaps.js
@@ -17,9 +17,9 @@ function Roadmaps() {
       <RoadmapCard link="https://miro.com/app/board/uXjVPEAGfM8=/?share_link_id=921354111487" img="frontend.jpg" category="Web-Development" heading="Front-End Devlopment" data="Everything you see on a website, like buttons, links, animations, and more, were created by a front end web developer. It is the front end developer's job to take the vision and design concept from the client and implement it through code."/>
       <RoadmapCard  img="backend.jpg" className="" category="Web-Development" heading="Back-End Devlopment" data="Everything you see on a website, like buttons, links, animations, and more, were created by a front end web developer. It is the front end developer's job to take the vision and design concept from the client and implement it through code."/>
       <RoadmapCard link="https://drive.google.com/file/d/1NkWD4_AjzjIdLgIHxp1K1Xm5LvXJ1l4r/view?usp=sharing" img="programming.jpg" category="CP" heading="Competitive-Programming" data="Competitive programming involves programmers competing against each other to solve programming questions in a limited amount of time. These events are usually held over the Internet or a local network. "/>
-      <RoadmapCard img="android.jpg" category="App Development" heading=" Android Devlopment" data="Everything you see on a website, like buttons, links, animations, and more, were created by a front end web developer. It is the front end developer's job to take the vision and design concept from the client and implement it through code."/>
-      <RoadmapCard img="flutter.webp" category="App Development" heading="Flutter " data="Everything you see on a website, like buttons, links, animations, and more, were created by a front end web developer. It is the front end developer's job to take the vision and design concept from the client and implement it through code."/>
-      <RoadmapCard img="native.png" category="App Development" heading="React-Native" data="Everything you see on a website, like buttons, links, animations, and more, were created by a front end web developer. It is the front end developer's job to take the vision and design concept from the client and implement it through code."/> 
+      <RoadmapCard img="android.jpg" category="App Development" heading=" Android Devlopment" data="Upcoming"/>
+      <RoadmapCard img="flutter.webp" category="App Development" heading="Flutter " data=" Upcoming"/>
+      <RoadmapCard img="native.png" category="App Development" heading="React-Native" data="Upcoming"/> 
     </div>
   </div>
 </section>
